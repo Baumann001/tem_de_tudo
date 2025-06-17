362857808a4d7637640031aa1131831ab40384a0
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-06-17 20:10:33 -0300

    -----

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function comprar() {
   alert("Você será redirecionado para a página do produto.");
-  window.location.href = "https://seulinkdeafiliado.com";
+  window.location.href = "https://s.shopee.com.br/3VYaKDcP2O";
 }
 
