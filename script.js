0a21f3a3d14676da913f9e868da95ad40b9a66cb
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-06-18 08:49:17 -0300

    add-

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-function comprar(url) {
-  alert("Você será redirecionado para a página do produto.");
-  window.location.href = url;
-}
+/*===== MENU SHOW =====*/ 
 
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("active");
-  }
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.classList.add("active");
-}
 
+/*===== REMOVE MENU MOBILE =====*/
