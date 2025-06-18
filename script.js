8d18dad2c36dbfc0bd6de481f83c66b5da962b9a
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-06-18 07:37:17 -0300

    add-

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ function comprar() {
   alert("Você será redirecionado para a página do produto.");
   window.location.href = "https://s.shopee.com.br/4VR8GxlqAJ";
 }
+function comprar() {
+  alert("Você será redirecionado para a página do produto.");
+  window.location.href = "https://s.shopee.com.br/4fkYU1fQ4O";
+}
 
