934c7613497a0e272a3eee0232fc33579129b4fb
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-06-18 07:29:55 -0300

    add-

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function comprar() {
   window.location.href = "https://s.shopee.com.br/3VYaKDcP2O";
 }
 
+
+function comprar() {
+  alert("Você será redirecionado para a página do produto.");
+  window.location.href = "https://s.shopee.com.br/4VR8GxlqAJ";
+}
+
